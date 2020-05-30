06fb9b1628b37e3038914787c6a41d56deda8473
Yol96 <you@example.com>
2020-05-31 01:09:02 +0300

    request fix

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -8,13 +8,12 @@ type User struct {
 	ID       int    `db:"user_id"`
 	Nickname string `db:"username"`
 	GroupID  int    `db:"group_id"`
-	Bantime  string `db:"srvbantime"`
 	Tag      string `db:"tag"`
 	TsID     string `db:"ts_id"`
 }
 
 func GetUserByTsId(teamspeakId string) (u User, err error) {
-	query := `SELECT user_id, username, group_id, srvbantime, tag, ts_id FROM phpbb_users WHERE ts_id = ?`
+	query := `SELECT user_id, username, group_id, tag, ts_id FROM phpbb_users WHERE ts_id = ?`
 	err = database.Db.Get(&u, query, teamspeakId)
 	return
 }
